d1b84c2a2d22866e323aa9ea7efdd51de0483ecc
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-26 14:23:07 -0300

    SIno added

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const Math = require('../src/math.js')
+const expect = require('chai').expect;
+const sinon = require('sinon');
 
 describe('Math class', () => {
     beforeEach(function() {
@@ -10,14 +12,46 @@ describe('Math class', () => {
         const math = new Math();
         this.timeout(4000);
 
-        math.sum(5, 5, (value) => {
-            assert.equal(value, 10);
+        //        math.sum(5, 5, (value) => {
+        //            assert.equal(value, 10);
+        //            done();
+        //        })
+        value = 5;
+
+        math.sum(value, 5, value => {
+            expect(value).to.equal(10);
             done();
         })
     });
 
     it('Multiply two numbers', function() {
         const math = new Math();
-        assert.equal(math.multiply(5, 5), 25);
+        const obj = {
+            name: 'Andre Ferriani',
+        };
+        const obj2 = {
+            name: 'Andre Ferriani',
+        };
+        //        assert.equal(math.multiply(5, 5), 25);
+        //expect(math.multiply(value, 5)).to.equal(0);
+        //expect(obj).to.have.property('name')
+        //    .to.equal('Andre Ferriani');
+        expect(obj).to.deep.equal(obj2);
+    });
+
+    it.only('Calls req with sum and index values', function() {
+        const req = {};
+        const res = {
+            load: function load() {
+                console.log('Called!');
+            }
+        };
+        //sinon.spy(res, 'load');
+        sinon.stub(res, 'load').returns('xpto');
+        const math = new Math();
+
+        math.printSum(req, res, 5, 5);
+
+        expect(res.load.args[0][0]).to.equal('index');
     });
 });
